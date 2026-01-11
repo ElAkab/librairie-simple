4b8a505f223bff855548c8519241fb6ab2a32dea
Adam <elkabir2005@gmail.com>
2026-01-11 11:20:15 +0100

    Commenter l'appel à dotenv.config() dans app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ import { fileURLToPath } from "url";
 // import dotenv from "dotenv";
 import cors from "cors";
 
-dotenv.config();
+// dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
